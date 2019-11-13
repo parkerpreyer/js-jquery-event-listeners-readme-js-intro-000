527a5e81e5549c718c32067149e93be2ad144b5b
Parker Preyer <parker.preyer@gmail.com>
2019-11-13 01:40:38 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,33 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert('Hey!');
+  });
+}
+
+function frameIt() {
+  $('img').load(function() {
+    $(this).addClass('tasty');
+  });
+}
+
+function submitIt() {
+  $('form').submit(function() {
+    alert('Your form is going to be submitted now.');
+  });
+}
+
+function pressIt() {
+  $('#typing').keydown(function(event) {
+    if(event.which === 71) {
+      alert('You pressed the G!');
+    }
+  });
+}
 
 $(document).ready(function(){
-
-// call functions here
-
+  getIt();
+  frameIt();
+  submitIt();
+  pressit();
 });
