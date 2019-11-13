69ed39beb277d8b25e6f249bb0b029b5516b66c1
Parker Preyer <parker.preyer@gmail.com>
2019-11-13 01:29:44 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -11,9 +11,13 @@ function frameIt() {
   });
 }
 
+function submitIt() {
+  $('form').submit(function() {
+    alert('Your form is going to be submitted now.');
+  });
+}
+
 $(document).ready(function(){
-
-getIt();
-frameIt();
-
+  getIt();
+  frameIt();
 });
